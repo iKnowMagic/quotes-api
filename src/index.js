cb0e7cca89d54045717aaeed30a19d38d4864f38
iknowmagic <ilan@iknowmagic.net>
2020-11-16 16:28:13 -0800

    port

--- src/index.js
+++ src/index.js
@@ -31,7 +31,7 @@ mongoose.connect(
 // Run the server!
 const start = async () => {
   try {
-    await fastify.listen(3000, '0.0.0.0')
+    await fastify.listen(process.env.PORT || 3000, '0.0.0.0')
     fastify.log.info(`server listening on '0.0.0.0'`)
   } catch (err) {
     fastify.log.error(err)
